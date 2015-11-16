6aa2987c93533ee8b426aca3cf877876b56441be
fatimaSoufiane <fatimaezzahrac@gmail.com>
2015-11-17 00:07:18 +0100

    Update City.java

--- src/entity/City.java
+++ src/entity/City.java
@@ -15,6 +15,7 @@ import displayer.Displayer;
 public class City {
 	protected String name;
 	protected List<Inhabitant> inhabitants;
+
 	/**
 	 * This postbox is used to distribute the letters
 	 */
@@ -47,7 +48,9 @@ public class City {
 						+ " euro\n");
 		letter.sender().debit(letter.cost());
 	}
-
+	/*
+	 * Distribution of letters from postbox of the city to each recipient
+	 */
 	public void distributeLetter() {
 		postbox.addAll(box);
 		box.removeAll(box);
@@ -71,6 +74,58 @@ public class City {
 	
 	public List<Letter<? extends Content>> box() {
 		return box;
+	}
+	/**
+	 * To the state bank accounts from each one the residents in the city
+	 */
+	public void getAccountCityState() {
+		for (Inhabitant hab : v) {
+			System.out.println("Bank account of Mr " + hab.getName() + " : "
+					+ hab.getBalance());
+		}
+	}
+
+	/**
+	 * Make the state the contents of the postbox displaying the sender the
+	 * receiver the type of letter
+	 */
+	public void getpostBoxState() {
+
+		System.out.println("############## POST BOX ##############");
+		List<Letter<?>> bag = new ArrayList<Letter<?>>(postbox);
+		int index = 1;
+		for (Letter<?> l : bag) {
+			System.out.println("Lettre N°" + index + " : Lettre ["
+					+ l.getClass() + "] of " + l.getSender().getName()
+					+ " is destined for " + l.getReceiver().getName());
+			index = index + 1;
+		}
+		System.out.println("######################################");
+	}
+
+	public int getNbHab() {
+		int nb = 0;
+		for (@SuppressWarnings("unused")
+		Inhabitant hab : v) {
+			nb = nb + 1;
+		}
+		return nb;
+	}
+
+	public int getNbLetters() {
+		int nb = 0;
+		for (@SuppressWarnings({ "rawtypes", "unused" })
+		Letter l : postbox) {
+			nb = nb + 1;
+		}
+		return nb;
+
+	}
+
+
+
+	public String getName() {
+		return name;
 	}
 
 }
