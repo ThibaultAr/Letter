4b97c94d6568cfe1493ed615141c339449218e32
ThibaultAr <thibault.arloing@hotmail.fr>
2015-11-17 17:48:37 +0100

    Revert "Update SimpleLetter.java"
    This reverts commit 3626525347c2443d70aebad8bee9b04c80046e65.

--- src/letter/SimpleLetter.java
+++ src/letter/SimpleLetter.java
@@ -4,28 +4,16 @@ import content.Text;
 import entity.Inhabitant;
 
 public class SimpleLetter extends Letter<Text> {
- protected Text text;
-	public Si protected T text;mpleLetter(Text content, Inhabitant sender, Inhabitant receiver) {
+
+	public SimpleLetter(Text content, Inhabitant sender, Inhabitant receiver) {
 		super(content, sender, receiver);
-		/* If the receiver is the sender then it is an acknowledgment of reception */
-        		if(receiver.getName()==sender.getName()){
-        			text = (Text) new Text("Acknowledgment of receipt");
-        			this.content=this.text;
-        		}
-        		else{
-        			String rslt = createLetter();
-                	text = (Text) new Text(rslt);
-        			this.content=this.text;
 	}
 
 	public String description() {
 		return "a simple " + super.description();
 	}
-	
 
 	public void doOnReceive() {
 		//nothing to do here 
-		System.out.println("Letter received in : "+this.getReceiver().getName()
-        			+" on the part of : "+this.getSender().getName());
 	}
 }
